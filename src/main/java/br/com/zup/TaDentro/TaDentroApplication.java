489c0a32886171d2f824c497ff88376107b0728b
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-15 17:01:45 -0300

    Code Base

---
+++ src/main/java/br/com/zup/TaDentro/TaDentroApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.TaDentro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaDentroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaDentroApplication.class, args);
+	}
+
+}
