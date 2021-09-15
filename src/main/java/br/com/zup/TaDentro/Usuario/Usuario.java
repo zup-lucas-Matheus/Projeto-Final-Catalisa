f118b74bd30cc01ad39aa199d50ec3cd6a9d0219
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-15 17:15:44 -0300

    Adicionando Classe Usuario

---
+++ src/main/java/br/com/zup/TaDentro/Usuario/Usuario.java
@@ -0,0 +1,39 @@
+package br.com.zup.TaDentro.Usuario;
+
+public class Usuario {
+
+    private String nome;
+    private String email;
+    private String cargo;
+
+    public Usuario() {
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+
+
+}
