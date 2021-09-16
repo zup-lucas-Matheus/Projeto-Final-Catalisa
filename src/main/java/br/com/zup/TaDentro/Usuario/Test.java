63efa4880de53280472461c01c4214ab92bbcd5a
leticia.reginajf <leticia.reginajf@gmail.com>
2021-09-16 13:46:48 -0300

    Incluindo Classe Usuário

--- src/main/java/br/com/zup/TaDentro/Usuario/Test.java
+++
@@ -1,5 +0,0 @@
-package br.com.zup.TaDentro.Usuario;
-
-public class Test {
-    private String teste;
-}
--- src/main/java/br/com/zup/TaDentro/Usuario/Usuario.java
+++ src/main/java/br/com/zup/TaDentro/Usuario/Usuario.java
@@ -1,4 +1,44 @@
 package br.com.zup.TaDentro.Usuario;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuario")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String nome;
+    private String email;
+    private String senha;
+
+
+    public Usuario(){
+
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
