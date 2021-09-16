b1f87cc415a8460a655289bb06c58b32b5f9cd8f
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-16 13:46:14 -0300

    Metódo para configuracao do Cors.

--- src/main/java/br/com/zup/TaDentro/security/ConfiguracaoDeSeguranca.java
+++ src/main/java/br/com/zup/TaDentro/security/ConfiguracaoDeSeguranca.java
@@ -1,24 +1,39 @@
 package br.com.zup.TaDentro.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
-
+    //Metodo que configura toda nossa aplicação
     protected void configuracao(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable();
+        httpSecurity.cors().configurationSource(configacaoDeCors());
 
         httpSecurity.authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/usuarios").permitAll()
                 .anyRequest().authenticated();
 
         httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
     }
+
+    //Metodo que configura o CORS.
+    @Bean
+    protected CorsConfigurationSource configacaoDeCors(){
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+
 
 }
