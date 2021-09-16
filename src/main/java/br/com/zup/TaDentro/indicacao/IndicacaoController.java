3bee76d97d573ded3de08a958ff7326a07f17761
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-16 14:17:28 -0300

    Classe Controller criada.

---
+++ src/main/java/br/com/zup/TaDentro/indicacao/IndicacaoController.java
@@ -0,0 +1,15 @@
+package br.com.zup.TaDentro.indicacao;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/indicacao")
+public class IndicacaoController {
+
+
+
+
+
+
+}
