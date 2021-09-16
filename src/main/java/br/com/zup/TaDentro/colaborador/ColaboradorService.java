f89981930b013fef8919488e874b687d1a894c34
alefe.nunes <alefe.nunes@zup.com.br>
2021-09-16 16:08:14 -0300

    Criar método para procurar colaborador pelo ID

--- src/main/java/br/com/zup/TaDentro/colaborador/ColaboradorService.java
+++ src/main/java/br/com/zup/TaDentro/colaborador/ColaboradorService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ColaboradorService {
@@ -11,21 +12,43 @@ public class ColaboradorService {
     @Autowired
     private ColaboradorRepository colaboradorRepository;
 
-    public Colaborador salvarColaborador (Colaborador colaborador) {
+    public Colaborador salvarColaborador(Colaborador colaborador) {
         return colaboradorRepository.save(colaborador);
     }
 
-    public List <Colaborador> exibirTodosOsColaboradores () {
+    public List<Colaborador> exibirTodosOsColaboradores() {
         return (List<Colaborador>) colaboradorRepository.findAll();
     }
 
-    public boolean colaboradorExistente (String id) {
+    public boolean colaboradorExistente(String id) {
         return colaboradorRepository.existsById(id);
     }
 
+    public Colaborador procurarSeColaboradorJaExiste(String id) {
+        Optional<Colaborador> colaboradorOptional = colaboradorRepository.findById(id);
 
+        if (colaboradorOptional.isPresent()) {
+            return colaboradorOptional.get();
+        }
 
+        throw new RuntimeException("Colaborador não trabalha aqui");
+
+    }
 
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
