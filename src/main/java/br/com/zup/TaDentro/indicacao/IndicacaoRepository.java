537193426b79ded4ed19b2f978d5fca7a647db65
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-16 13:59:31 -0300

    Interface Repository criada.

---
+++ src/main/java/br/com/zup/TaDentro/indicacao/IndicacaoRepository.java
@@ -0,0 +1,9 @@
+package br.com.zup.TaDentro.indicacao;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface IndicacaoRepository extends CrudRepository<Indicacao, Integer> {
+
+
+
+}
