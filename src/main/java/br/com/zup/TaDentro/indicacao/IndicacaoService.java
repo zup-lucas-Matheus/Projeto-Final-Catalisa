7c0e368138adc8b72f52d445a474f801c32b25a1
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-16 14:04:08 -0300

    Metódo para cadastrar indicado.

--- src/main/java/br/com/zup/TaDentro/indicacao/IndicacaoService.java
+++ src/main/java/br/com/zup/TaDentro/indicacao/IndicacaoService.java
@@ -2,12 +2,18 @@ package br.com.zup.TaDentro.indicacao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.persistence.criteria.CriteriaBuilder;
+
 @org.springframework.stereotype.Service
 public class IndicacaoService {
 
     @Autowired
     private IndicacaoRepository indicacaoRepository;
 
+    //Metódo para cadastrar indicação.
+    public Indicacao saveIndicacao(Indicacao indicado){
+        return indicacaoRepository.save(indicado);
+    }
 
 
 }
