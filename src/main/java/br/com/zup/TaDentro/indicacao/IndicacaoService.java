ea27b394dd2d6248650f60a0a1693b07e697f009
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-16 14:02:26 -0300

    Adicionando injeção da repository na classe service.

--- src/main/java/br/com/zup/TaDentro/indicacao/IndicacaoService.java
+++ src/main/java/br/com/zup/TaDentro/indicacao/IndicacaoService.java
@@ -1,9 +1,12 @@
 package br.com.zup.TaDentro.indicacao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 @org.springframework.stereotype.Service
 public class IndicacaoService {
 
-
+    @Autowired
+    private IndicacaoRepository indicacaoRepository;
 
 
 
