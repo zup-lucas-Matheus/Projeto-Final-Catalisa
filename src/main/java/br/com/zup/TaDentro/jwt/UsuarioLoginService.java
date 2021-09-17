6899dea538d997ef416dce2aaa5c1d33f1c598c9
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-17 17:27:41 -0300

    Classe UsuarioLoginService criada.

---
+++ src/main/java/br/com/zup/TaDentro/jwt/UsuarioLoginService.java
@@ -0,0 +1,16 @@
+package br.com.zup.TaDentro.jwt;
+
+import br.com.zup.TaDentro.Usuario.Usuario;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioLoginService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
+}
