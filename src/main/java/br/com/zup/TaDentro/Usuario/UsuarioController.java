785ceb052d266407eb03013510e1655544a7a47f
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-18 20:46:45 -0300

    Injeção do modelMapper na Controller

--- src/main/java/br/com/zup/TaDentro/Usuario/UsuarioController.java
+++ src/main/java/br/com/zup/TaDentro/Usuario/UsuarioController.java
@@ -1,5 +1,7 @@
 package br.com.zup.TaDentro.Usuario;
 
+import br.com.zup.TaDentro.Usuario.dto.UsuarioDto;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +14,14 @@ import java.util.List;
 public class UsuarioController {
 
     @Autowired
-    public UsuarioService service;
+    private UsuarioService service;
+    @Autowired
+    private ModelMapper modelMapper;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Usuario cadastroUsuario(@RequestBody @Valid Usuario usuario){
+    public Usuario cadastroUsuario(@RequestBody @Valid UsuarioDto usuario){
+
         return service.salvarUsuario(usuario);
     }
 
