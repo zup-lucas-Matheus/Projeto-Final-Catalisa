b76b7dcbd21c8e8afe87677953ae523d44e98a78
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-20 16:25:53 -0300

    Classe controller advisor para colaborador.

---
+++ src/main/java/br/com/zup/TaDentro/colaborador/exceptionColaborador/ControllerAdvColaborador.java
@@ -0,0 +1,11 @@
+package br.com.zup.TaDentro.colaborador.exceptionColaborador;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvColaborador {
+
+
+
+
+}
