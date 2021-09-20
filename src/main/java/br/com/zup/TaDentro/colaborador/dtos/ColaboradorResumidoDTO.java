e27aac77928b5c2b22a9244c232c4c7b2551b760
alefe.nunes <alefe.nunes@zup.com.br>
2021-09-20 14:20:45 -0300

    Criar dto de colaborador

---
+++ src/main/java/br/com/zup/TaDentro/colaborador/dtos/ColaboradorResumidoDTO.java
@@ -0,0 +1,52 @@
+package br.com.zup.TaDentro.colaborador.dtos;
+
+import br.com.zup.TaDentro.enums.Cargo;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+public class ColaboradorResumidoDTO {
+
+    @NotNull
+    private String nome;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Cargo cargo;
+    @NotNull
+    private LocalDate dataContratacao;
+
+    public ColaboradorResumidoDTO() {
+    }
+
+    public ColaboradorResumidoDTO(String nome, Cargo cargo, LocalDate dataContratacao) {
+        this.nome = nome;
+        this.cargo = cargo;
+        this.dataContratacao = dataContratacao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Cargo getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(Cargo cargo) {
+        this.cargo = cargo;
+    }
+
+    public LocalDate getDataContratacao() {
+        return dataContratacao;
+    }
+
+    public void setDataContratacao(LocalDate dataContratacao) {
+        this.dataContratacao = dataContratacao;
+    }
+
+}
