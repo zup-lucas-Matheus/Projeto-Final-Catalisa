c0ec1e9dcf8c8401924f45b1a9ba9f2757e68ba1
alefe.nunes <alefe.nunes@zup.com.br>
2021-09-21 14:48:49 -0300

    Implantar classe erro bad request indicação

--- src/main/java/br/com/zup/TaDentro/indicacao/exceptionIndicacao/ControllerAdvIndicacao.java
+++ src/main/java/br/com/zup/TaDentro/indicacao/exceptionIndicacao/ControllerAdvIndicacao.java
@@ -1,9 +1,19 @@
 package br.com.zup.TaDentro.indicacao.exceptionIndicacao;
 
+import br.com.zup.TaDentro.Usuario.exceptionUsuario.ErroBadRequestUsuario;
+import br.com.zup.TaDentro.Usuario.exceptionUsuario.MensagemErroUsuario;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllerAdvIndicacao {
 
-
+    public ResponseEntity<ErroBadRequestUsuario> handlerBadRequest(MensagemErroUsuario ex){
+        return new ResponseEntity<>(
+                ErroBadRequestUsuario.builder()
+                        .titulo("BAD REQUEST")
+                        .mensagem(ex.getMessage())
+                        .StatusCode(400)
+                        .build(), HttpStatus.BAD_REQUEST);
 }
