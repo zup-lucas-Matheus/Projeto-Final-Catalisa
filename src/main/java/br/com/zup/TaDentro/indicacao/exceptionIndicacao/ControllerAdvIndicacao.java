a168b98d4019a8ab08663ed5459b86bd6d375e8b
alefe.nunes <alefe.nunes@zup.com.br>
2021-09-21 14:41:02 -0300

    Implantar classe mensagem de erro indicação

---
+++ src/main/java/br/com/zup/TaDentro/indicacao/exceptionIndicacao/ControllerAdvIndicacao.java
@@ -0,0 +1,9 @@
+package br.com.zup.TaDentro.indicacao.exceptionIndicacao;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvIndicacao {
+
+
+}
