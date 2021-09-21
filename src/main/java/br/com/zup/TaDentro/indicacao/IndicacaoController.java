fcbe4a653639164ca1c688ca2dcd1161ac7bb613
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-21 16:30:31 -0300

    EndPoint atualizado.

--- src/main/java/br/com/zup/TaDentro/indicacao/IndicacaoController.java
+++ src/main/java/br/com/zup/TaDentro/indicacao/IndicacaoController.java
@@ -2,9 +2,11 @@ package br.com.zup.TaDentro.indicacao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/indicacao")
@@ -15,9 +17,24 @@ public class IndicacaoController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public Indicacao cadastrarIndicacao(@RequestBody @Valid Indicacao indicacao){
-        return indicacaoService.saveIndicacao(indicacao);
+    public Indicacao cadastrarIndicacao(@RequestBody @Valid Indicacao indicacao, Authentication authentication){
+        String cpf = authentication.getName();
+        return indicacaoService.saveIndicacao(cpf, indicacao);
     }
 
+    @GetMapping
+    public List<Indicacao> indicacaoList(){
+        return indicacaoService.indicacaoList();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarIndicacao(int id){
+        indicacaoService.deleteIndicacao(id);
+    }
+
+    @PutMapping
+    public void atualizarIndicacao(@RequestBody Indicacao indicacao){
+        indicacaoService.atualizarIndicacao(indicacao);
+    }
 
 }
