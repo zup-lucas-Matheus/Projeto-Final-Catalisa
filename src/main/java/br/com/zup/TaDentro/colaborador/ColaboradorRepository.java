ecba77ffca4079109fb4782287aa5e42b29091db
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-22 11:34:14 -0300

    Metódo para buscar o usuário

--- src/main/java/br/com/zup/TaDentro/colaborador/ColaboradorRepository.java
+++ src/main/java/br/com/zup/TaDentro/colaborador/ColaboradorRepository.java
@@ -1,5 +1,6 @@
 package br.com.zup.TaDentro.colaborador;
 
+import br.com.zup.TaDentro.Usuario.Usuario;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
@@ -8,5 +9,7 @@ import java.util.Optional;
 public interface ColaboradorRepository extends CrudRepository <Colaborador , Integer> {
 
     Optional<Colaborador> findByCpf(String cpf);
+
+    Optional<Colaborador> findByLoginUsuario(Usuario usuario);
 
 }
