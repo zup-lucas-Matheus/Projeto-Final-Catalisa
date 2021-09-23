1abdcf1a0f3bf7a0892265f1a783122e98404777
alefe.nunes <alefe.nunes@zup.com.br>
2021-09-23 17:16:10 -0300

    Consertar método de atualizar colaborador

--- src/main/java/br/com/zup/TaDentro/colaborador/ColaboradorController.java
+++ src/main/java/br/com/zup/TaDentro/colaborador/ColaboradorController.java
@@ -1,8 +1,10 @@
 package br.com.zup.TaDentro.colaborador;
 
 
+import br.com.zup.TaDentro.colaborador.dtos.ColaboradorPUTDto;
 import br.com.zup.TaDentro.colaborador.dtos.ColaboradorResumidoDTO;
 import br.com.zup.TaDentro.indicacao.Indicacao;
+import br.com.zup.TaDentro.indicacao.dtos.IndicacaoPUTDto;
 import br.com.zup.TaDentro.indicacao.dtos.IndicacaoPesquisaDto;
 import br.com.zup.TaDentro.indicacao.dtos.IndicacaoResumidaDTO;
 import br.com.zup.TaDentro.jwt.filter.JwtComponent;
@@ -64,14 +66,16 @@ public class ColaboradorController {
 
 
     @PutMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Colaborador atualizarColaborador() {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizarColaborador(@RequestBody ColaboradorPUTDto colaboradorPUTDto) {
 
-        ColaboradorResumidoDTO colaboradorResumidoDTO = new ColaboradorResumidoDTO();
-        return modelMapper.map(colaboradorResumidoDTO, Colaborador.class);
+        Colaborador colaboradorModel = modelMapper.map(colaboradorPUTDto, Colaborador.class);
+
+        colaboradorService.atualizarColaborador(colaboradorModel);
+
     }
 
-    /*@GetMapping
+   /*@GetMapping
     public List<IndicacaoResumidaDTO> indicacaoList(@RequestBody IndicacaoPesquisaDto indicacaoPesquisaDto, Authentication authentication) {
         String email = authentication.getName();
         List<Indicacao> retorno = colaboradorService.pesquisaPorData(email, indicacaoPesquisaDto.getDataInicial(), indicacaoPesquisaDto.getDataFinal());
