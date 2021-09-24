c83ec7a0930c81c0ae4a9634810119cc4cba3209
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-23 21:06:58 -0300

    Metódo para buscar colaborador.

--- src/main/java/br/com/zup/TaDentro/indicacao/IndicacaoRepository.java
+++ src/main/java/br/com/zup/TaDentro/indicacao/IndicacaoRepository.java
@@ -12,5 +12,7 @@ public interface IndicacaoRepository extends CrudRepository<Indicacao, Integer> 
     Optional<Indicacao> findByCpf(String cpf);
 
 
-    List<Indicacao> findByColaboradorAndDataDeCadastroBetween(Colaborador colaborador, LocalDate dataInicial, LocalDate dataFinal);
+    List<Indicacao> findByColaborador(Colaborador colaborador);
+
+
 }
