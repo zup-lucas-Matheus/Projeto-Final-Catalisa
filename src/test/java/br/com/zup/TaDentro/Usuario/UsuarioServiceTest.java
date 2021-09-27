4b1adcb296f896c3a871be083e65e3cb6c019f2f
leticia.reginajf <leticia.reginajf@gmail.com>
2021-09-27 18:03:41 -0300

    Correção de Teste Unitário - Usuários

--- src/test/java/br/com/zup/TaDentro/Usuario/UsuarioServiceTest.java
+++ src/test/java/br/com/zup/TaDentro/Usuario/UsuarioServiceTest.java
@@ -2,31 +2,21 @@
 package br.com.zup.TaDentro.Usuario;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.stereotype.Repository;
-
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
-@WebMvcTest
 public class UsuarioServiceTest {
 
-    @Autowired
-    private UsuarioServiceTest usuarioServiceTest;
-
-    @Autowired
-    private UsuarioService service;
 
     @MockBean
-    private UsuarioRepository repository;
+    private UsuarioRepository usuarioRepository;
 
     @Autowired
     private UsuarioService usuarioservice;
@@ -37,7 +27,7 @@ public class UsuarioServiceTest {
 
         Usuario user = new Usuario();
 
-        Mockito.when(repository.save(Mockito.any(Usuario.class)))
+        Mockito.when(usuarioRepository.save(Mockito.any(Usuario.class)))
                 .thenReturn(user);
 
         Usuario usuarioTest = usuarioservice.salvarUsuario(user);
@@ -45,14 +35,46 @@ public class UsuarioServiceTest {
     }
 
     @Test
-    public void testUsuario(){
+    public void testarMetodoQueRetornaTodosOsUsuarios(){
         Usuario usuario = new Usuario();
 
         List<Usuario> usuarios = Arrays.asList(usuario);
-        Mockito.when(repository.findAll()).thenReturn(usuarios);
+        Mockito.when(usuarioRepository.findAll()).thenReturn(usuarios);
 
         List<Usuario> usuariosTest = Arrays.asList(usuario);
         Assertions.assertEquals(usuarios, usuarioservice.exibirUsuario());
+    }
+
+    @Test
+    public void testarMetodoEncontrarUsuarioPorIdPositivo() throws Exception {
+        Usuario usuario = new Usuario();
+        Optional<Usuario> usuarioOptional = Optional.of(usuario);
+
+        Mockito.when(usuarioRepository.findById(Mockito.anyInt())).thenReturn(usuarioOptional);
+        Assertions.assertEquals(usuario, usuarioservice.encontrarUsuarioPorEmail("leticia@oi.com.br"));
+    }
+
+    @Test
+    public void testarMetodoEncontrarUsuarioPorIdNegativoParaLancaExecao() throws Exception {
+        Usuario usuario = new Usuario();
+        Optional<Usuario> usuarioOptional = Optional.empty();
+
+        Mockito.when(usuarioRepository.findById(Mockito.anyInt())).thenReturn(usuarioOptional);
+
+        RuntimeException exception = Assertions
+                .assertThrows(RuntimeException.class, () -> {usuarioservice.encontrarUsuarioPorEmail("324e3");});
+
+        Assertions.assertTrue(exception.getMessage().equals("Usuario não encontrado"));
+
+    }
+
+    @Test
+    public void testarMetodoParaExcluirUsuarioPorIdPositivo() throws Exception {
+        Usuario usuario = new Usuario();
+        Mockito.when(usuarioRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(usuario));
+        usuarioservice.deletarUsuario(1234567);
+
+        Mockito.verify(usuarioRepository).delete(usuario);
     }
 
 
